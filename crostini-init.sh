a33cac2eec7a2a46e608f9072b4cdfe27191045d
David Mitchell <monch1962@gmail.com>
2020-03-06 07:41:02 +1100

    Update crostini-init.sh

--- crostini-init.sh
+++ crostini-init.sh
@@ -11,7 +11,7 @@ get_python() {
 get_powershell() {
 	sudo apt install curl gnupg apt-transport-https liblttng-ust-ctl2 liblttng-ust0 libunwind8 liburcu4 wget
 	curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-	sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-stretch-prod stretch main" > /etc/apt/sources.list.d/microsoft.list'
+	sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-buster-prod buster main" > /etc/apt/sources.list.d/microsoft.list'
 	sudo apt-get update
 	sudo apt-get install -y powershell
 	sudo apt-get install -f
